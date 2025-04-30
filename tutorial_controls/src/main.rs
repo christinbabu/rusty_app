47800b626d95fcbd3fa194839fe696434cf1b190
christin <johnchristinchriz@gmail.com>
2025-04-30 21:15:41 +0530

    Functions tutorials

---
+++ tutorial_controls/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let x = five();
+    let y = plus_five(x);
+    let sentence = "The_typed numbers are";
+    let z= make_a_sentence(x, y, sentence);
+    println!("The value of x is {},{},{}", x, y, z);
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_five(x: i32) -> i32 {
+    x + 5
+}
+
+fn make_a_sentence(a: i32, b: i32, sen: &str) -> String {
+    format!("{},{},{}", sen, a, b)
+}
